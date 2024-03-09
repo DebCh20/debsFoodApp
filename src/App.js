f9bd6bd05204b4ce795848376081107e1fb304f3
Deb <debasish.chandra@accenture.com>
2024-03-09 08:42:19 +0530

    added the card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Dispcard from './Dispcard';
+import Foodmenu from './Foodmenu';
 function App() {
   return (
    <>
     <Navbar></Navbar>
+    <Foodmenu></Foodmenu>
    </>
   );
 }
---
+++ src/Foodmenu.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Dispcard from './Dispcard';
+
+function Foodmenu() {
+  return (
+    <div>
+        <h2>asian foods</h2>
+        <br/>
+        <Dispcard></Dispcard>
+        <h2>asian foods</h2>
+        <br/>
+        <Dispcard></Dispcard>
+        <h2>asian foods</h2>
+        <br/>
+        <Dispcard></Dispcard>
+        <h2>asian foods</h2>
+        <br/>
+        <Dispcard></Dispcard>
+    </div>
+  )
+}
+
+export default Foodmenu
