2d52edfa492ffedb053fc63db0cf8230e4e2d48b
Debasish <101311620+DebCh20@users.noreply.github.com>
2024-03-10 20:34:30 +0530

    changes added to homepage

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Navbar from './Navbar';
 import Dispcard from './Dispcard';
 import Foodmenu from './Foodmenu';
+import Homepage from './homepage/Homepage';
 function App() {
   return (
    <>
     <Navbar></Navbar>
-    <Foodmenu></Foodmenu>
+    <Homepage/>
    </>
   );
 }
