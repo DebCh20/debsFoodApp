60d9bc21054fdc253d48e7a1effee7bc487c49e1
Debasish <101311620+DebCh20@users.noreply.github.com>
2024-03-14 09:33:09 +0530

    added the fetch url for fetching the recipes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 // import './output.css'
 function Navbar() {
   const navigate = useNavigate();
-
-  const searchRecipe= () => {
-    navigate('/findRecipe');
+  let [query, setQuery]= useState('')
+  const searchRecipe= () => {    
+    setQuery(document.querySelector('.searchBar').value);
+    navigate(`/findRecipe/${query}`);
   };
+ 
+  useEffect(()=>{
+     const fetchRecipe = async ()=>{
+      const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=6b734143b21846be87a110e3e8931c8f&query=${query}`);
+      const data= await response.json();
+      console.log('data', data);
+     }
+     fetchRecipe();
+  },[query])
+
   return (
     <div className='navBar'>
         <Link className='homepagelink' to='/' style={{textDecoration:"none"}}>Home</Link>       
--- src/Showrecipepage/Showrecipepage.jsx
+++ src/Showrecipepage/Showrecipepage.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-
+import './Showrecipepage.css';
 function Showrecipepage() {
-  let index=10;  
+  let index=10; 
+  let recipeList = ['pizza','burger', 'coke','pizza','burger', 'coke','pizza','burger', 'coke','pizza','burger', 'coke'] 
   return (
     <div className='recipeCardsContainer'>
+      {recipeList.map((recipe, index) => (
       <div className='recipeCard' style={{display:'flex', flexDirection:'column',
-       alignItems:'center', height:'15rem', width:'13rem', border:'2px solid'}}>
-        <img src='http://forkify-api.herokuapp.com/images/best_pizza_dough_recipe1b20.jpg' style={{height:'80%', width:'100%'}}></img>
+       alignItems:'center', height:'15rem', width:'13rem', border:'2px solid', marginTop:'2rem'}}>
+        <img src='https://spoonacular.com/recipeImages/642539-312x231.png' style={{height:'80%', width:'100%'}}></img>
         <a href="http://www.101cookbooks.com/archives/001199.html" style={{textDecoration:'none'}}>Best Pizza Dough Ever</a>
       </div> 
+      ))}
     </div>
   )
 }
