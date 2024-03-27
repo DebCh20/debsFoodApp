f51af8c1f745aef63957938f1c44364030023397
Debasish <101311620+DebCh20@users.noreply.github.com>
2024-03-27 20:01:23 +0530

    added the new homepage

---
+++ src/LandingPage/Landingpage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Landingpage() {
+  return (
+    <div>
+        <div style={{display:'flex', alignItems:'center', gap:'10rem', marginTop:'20px'}}>
+            <img src='https://images.unsplash.com/photo-1515003197210-e0cd71810b5f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGZvb2R8ZW58MHwwfDB8fHww'></img>
+            <h1 style={{textAlign:'center', width:'12rem'}}>Hungry ??!! <br/> Dont't worry we got you covered !!</h1>
+        </div>
+        <div style={{display:'flex', alignItems:'center', gap:'10rem', marginTop:'20px'}}>            
+            <h1 style={{textAlign:'center', width:'6rem', marginLeft:'10rem'}}>Pistachios aren’t nuts—they are actually fruits.</h1>
+            <img src='https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZvb2R8ZW58MHwwfDB8fHww'></img>
+        </div> 
+
+    </div>
+  )
+}
+
+export default Landingpage
